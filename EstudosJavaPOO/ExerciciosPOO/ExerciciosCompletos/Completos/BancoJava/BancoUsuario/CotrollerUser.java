4a6cfde2c29f03cbf77beea3ba215db330b6be3c
Vtor Gabriel <Vitor.desenvolvedor18@gmail.com>
2026-06-13 01:19:06 -0300

    primeiro commit

---
+++ EstudosJavaPOO/ExerciciosPOO/ExerciciosCompletos/Completos/BancoJava/BancoUsuario/CotrollerUser.java
@@ -0,0 +1,18 @@
+package LogicaJava.ExerciciosCompletos.Completos.BancoJava.BancoUsuario;
+
+import LogicaJava.ExerciciosCompletos.Completos.ContaBancaria.Dominio.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CotrollerUser {
+    private List<Usuario> usuarios = new ArrayList<>();
+
+
+
+
+
+
+}
+
+
